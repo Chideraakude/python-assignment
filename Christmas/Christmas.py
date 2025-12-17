ab53a23454abdf8a0308f9df246c7dfe68a283d5
Chideraakude <akudechidera@gmail.com>
2025-12-17 05:39:19 +0100

    Christmas Songs

---
+++ Christmas/Christmas.py
@@ -0,0 +1,131 @@
+for day in range(1, 13):
+
+    match day:
+        case 1:
+            print("\nOn the first day of Christmas my true love sent to me:")
+            print("A partridge in a pear tree.")
+
+        case 2:
+            print("\nOn the second day of Christmas my true love sent to me:")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+        case 3:
+            print("\nOn the third day of Christmas my true love sent to me:")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+        case 4:
+            print("\nOn the fourth day of Christmas my true love sent to me:")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+        case 5:
+            print("\nOn the fifth day of Christmas my true love sent to me:")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+        case 6:
+            print("\nOn the sixth day of Christmas my true love sent to me:")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+        case 7:
+            print("\nOn the seventh day of Christmas my true love sent to me:")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+
+        case 8:
+            print("\nOn the eighth day of Christmas my true love sent to me:")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+
+        case 9:
+            print("\nOn the ninth day of Christmas my true love sent to me:")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+
+        case 10:
+            print("\nOn the tenth day of Christmas my true love sent to me:")
+            print("Ten lords a-leaping,")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+
+        case 11:
+            print("\nOn the eleventh day of Christmas my true love sent to me:")
+            print("Eleven pipers piping,")
+            print("Ten lords a-leaping,")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+
+        case 12:
+            print("\nOn the twelfth day of Christmas my true love sent to me:")
+            print("Twelve drummers drumming,")
+            print("Eleven pipers piping,")
+            print("Ten lords a-leaping,")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.")
+
+
+
+
---
+++ Christmas/Creation.py
@@ -0,0 +1,37 @@
+
+
+for Day in range (1,8):
+
+        match Day:
+            case 1:
+                print("\nOn the first day of Creation: ")
+                print("God created Light")
+
+
+            case 2:
+                print("\nOn the second day of Creation: ")
+                print("God seperated the firmament of the heavens from the firmament of the earth")
+
+
+            case 3:
+                print("\nOn the third day of Creation: ")
+                print("God created seperated the land from the sea and plants")
+
+
+            case 4:
+                print("\nOn the fourth day of Creation: ")
+                print("God created two great light, the light that rules the day he called, sun, and the liht that rules the night he called, moon, he also created the stars")
+
+
+            case 5:
+                print("\nOn the fifth day of Creation: ")
+                print("God created every bird of the air and all sea creatures")
+
+            case 6:
+                print("\nOn the sixth day of Creation: ")
+                print("God created every living creatures and he created man in his image and likeness")
+
+
+            case 7:
+                print("\nOn the seventh day of Creation: ")
+                print("God rested from all the work he has done")
