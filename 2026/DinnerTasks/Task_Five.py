f308e5798b65b19c2ca7b3cd7771ceec9c9d24e5
Chideraakude <akudechidera@gmail.com>
2026-02-24 05:56:38 +0100

    Dinner Task

---
+++ 2026/DinnerTasks/Task_Five.py
@@ -0,0 +1,4 @@
+for num in range (1, 51):
+    print("\n Print all multiples of 3 between 1 and 50")
+    if(num % 3 == 0):
+        print(num)
---
+++ 2026/DinnerTasks/dinner_Task.py
@@ -0,0 +1,317 @@
+#def task_One():
+#    print("\nPrint Numbers From 100 to 1")
+#    for number in range(100, 0, -1):
+#        print(number)
+#    print()
+#
+#task_One()
+
+def task_Two():
+    print("\nPrint all odd numbers between 50 abd 100")
+
+    for number in range(51, 100):
+
+        if number % 2 != 0:
+            print(number)
+
+task_Two()
+
+
+
+def task_Three():
+    print("\nPrint all numbers from 100 down to 1")
+
+for number in range(100, 0, -1):
+    print (number)
+
+task_Three()
+
+#    for ( int count = 50; count < 101; count++){
+#
+#        if( count % 2 != 0){
+#        System.out.println("Odd Numbers: " + count);
+
+
+#def task_Four():
+#    print("\nPrint the square of numbers from 1 to 20")
+#    for num in range (1, 21):
+#       print(num ** 2)
+#    print()
+#
+#task_Four()
+#
+#
+#def task_Five():
+#    print("\nPrint all multiples of 3 between 1 and 50")
+#    for num in range(1,51):
+#        if (num % 3 == 0):
+#            print(num)
+#    print()
+#
+#task_Five()
+#
+#
+#
+#
+#def task_Six():
+#    print("\nPrint all numbers between 1 and 100 that are divisible by both 3 and 5")
+#    for number in range (1, 101):
+#        if(number % 3 == 0 and number % 5 == 0):
+#            print(number)
+#    print()
+#
+#task_Six()
+#
+#
+#
+#def task_Seven():
+#    sum = 0
+#
+#    print("\nCount how many numbers between 1 and 100 are diviible by 7")
+#    for num in range(1, 101):
+#        if num % 7 == 0:
+#            sum+=1
+#        print(sum)
+#        
+#    print()
+#
+#task_Seven()
+#
+#
+#
+#def task_Eight():
+#    ("\nCalaculate and print the sum of the first 50 natural numbers")
+#
+#    total = 0
+#    for num in range(1, 51):
+#        total += num
+#        print(total)
+#    print()clear
+#
+#task_Eight()
+#
+#
+#def task_Nine():
+#    print("\n Print the product of the first 10 natural numbers")
+#    product = 1
+#    for num in range(1,11):
+#        product *= num
+#    print(product)
+#
+#task_Nine()
+#
+#
+#
+#def task_Ten():
+#    print("\nPrint all letters from A to Z")
+#    for letter in range (65, 91):
+#        print(chr(letter))
+#
+#task_Ten()
+#
+#
+#def task_Eleven():
+#
+#    number = 10
+#
+#    for times in range(1,13):
+#        print(f"{number} * { times} = {number * times}")
+#
+#task_Eleven()
+#
+#
+#def task_Twelve():
+#    print('\nPrint all characters in a string individually')
+#    name = "Chidera"
+#    for char in name:
+#        print(char)
+#task_Twelve()
+#
+#
+#def task_Thirtheen():
+#    ("\n Count how many times 'e' appears in a string")
+#    text = "Chidera AKude ChidiebereeE"
+#    count = 0
+#    for cha in text:
+#         if cha == 'e' or cha == 'E':
+#            count += 1
+#    print(count)
+#
+#task_Thirtheen()
+#
+#
+#def task_Fourteen():
+#    ("\nConvert Lowercase letter of a string to uppercase")
+#    text = "hello chidera"
+#    
+#    upper_Case_Text = text.upper()
+#    print(upper_Case_Text)
+#
+#task_Fourteen() 
+#
+#
+#
+#
+#def task_Fifteen():
+#    print("\nConvert Lowercase letter of a string to uppercase")
+#    text = "HELLO CHIDERA"
+#    
+#    lower_Case_Text = text.lower()
+#    print(lower_Case_Text)
+#
+#task_Fifteen() 
+#
+#
+#def task_Sixteen():
+#    print("\n Count how many vowels are in a iven string")
+#    text = "Chidera akude"
+#    count = 0
+#
+#    for character in text:
+#        if character == "a" or character == "i" or character == "o" or character == "u" or character == "e":
+#            count += 1
+#    print(count)
+#
+#task_Sixteen()
+#
+#
+#
+#def task_Seventeen():
+#
+#    print("\nCount how many digit are in a given number")
+#    number = 1234567890
+#    count = 0
+#
+#    for num in str(number):
+#        count +=1
+#    print(count)
+#
+#
+#task_Seventeen()
+#
+#
+#def task_Eighteen():
+#
+#    print("\nsum all digit in a given number")
+#    number = 1234567890
+#    count = 0
+#    total = 0
+#
+#    for num in str(number):
+#        total += int(num)
+#    print(total)
+#
+#
+#task_Eighteen()
+#
+#
+#
+#
+#def task_Nighteen():
+#
+#    print("\nFind the largest a given number")
+#    number = 1234567890
+#    largest = 0
+#
+#    for num in str(number):
+#        num = int(num)
+#        if num > largest:
+#            largest = num
+#    print(largest) 
+#
+#
+#task_Nighteen()
+#
+#
+#
+#def task_Twenty():
+#
+#    print("\nFind the smallest a given number")
+#    number = 1234567890
+#    smallest = 9
+#
+#    for num in str(number):
+#        num = int(num)
+#        if num < smallest:
+#            smallest = num
+#    print(smallest) 
+#
+#
+#task_Twenty()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#    
+#    
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
